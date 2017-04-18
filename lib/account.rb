d8cdaf8a2dd61046d467e621c0a8519adcf2992d
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-04-18 17:10:34 +0100

    Add deposit method that changes balance

--- lib/account.rb
+++ lib/account.rb
@@ -5,4 +5,9 @@ class Account
   def initialize
     @balance = 0
   end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,7 +2,7 @@ require 'account'
 
 describe Account do
 
-  context "new_account" do
+  context "new account" do
     it "should be instatiated with 0 balance" do
       expect(subject.balance).to eq 0
     end

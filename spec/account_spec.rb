9103be86914e1d0ad4bf117e2993daa3cf3bd2dd
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-04-18 17:07:22 +0100

    Write test for deposit method

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,4 +7,10 @@ describe Account do
       expect(subject.balance).to eq 0
     end
   end
+
+  context "deposit" do
+    it "should add an amount to an account" do
+      expect{subject.deposit(1000)}.to change{subject.balance}.by(1000)
+    end
+  end
 end

cdcdca3a15baad1bb24cca37d070f1ab361d5c3c
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-04-18 20:45:49 +0100

    Move print_statement to statement class

---
+++ lib/statement.rb
@@ -0,0 +1,15 @@
+class Statement
+
+  def initialize(transactions)
+    @transactions = transactions
+  end
+
+  def print_statement
+    statement = ''
+    @transactions.each do |transaction|
+      statement += "#{transaction[:date]} || #{sprintf '%.2f', transaction[:credit] if transaction[:credit]} || #{sprintf '%.2f', transaction[:debit] if transaction[:debit]} || #{sprintf '%.2f', transaction[:balance]}\n"
+    end
+    "date || credit || debit || balance\n" + statement
+  end
+
+end
